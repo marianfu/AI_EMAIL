23fb2e4ef589ba8f0ae43520149d028df48c3867
marianfu <marianfurriel@gmail.com>
2015-11-07 11:33:44 -0300

    rest

--- AI_EMAIL_EJB/ejbModule/beans/LogBean.java
+++ AI_EMAIL_EJB/ejbModule/beans/LogBean.java
@@ -1,9 +1,15 @@
 package beans;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import com.google.gson.Gson;
+
+import entities.Log;
 
 /**
  * Session Bean implementation class LogBean
@@ -12,16 +18,39 @@ import javax.persistence.PersistenceContext;
 @LocalBean
 public class LogBean {
 
-    @PersistenceContext (unitName = "CRM")
-    private EntityManager em;
-	
-    public LogBean() {
-        // TODO Auto-generated constructor stub
-    }
-    
-    public String getLogs(){
-    	
-    	
-    }
+	@PersistenceContext(unitName = "CRM")
+	private EntityManager em;
+
+	public LogBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getLogs() {
+
+		String logs = null;
+		try {
+			List<Log> lista = (List<Log>) em.createQuery("select m from Log m").getResultList();
+			Gson gson = new Gson();
+			logs = gson.toJson(lista);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return logs;
+	}
+
+	public String getLogsPorModulo(String modulo) {
+
+		String logs = null;
+		try {
+			List<Log> lista = (List<Log>) em.createQuery("select m from Log m where m.modulo = ?").setParameter(1, modulo).getResultList();
+			Gson gson = new Gson();
+			logs = gson.toJson(lista);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return logs;
+	}
 
 }
--- AI_EMAIL_WEB/src/rest/LogRESTService.java
+++ AI_EMAIL_WEB/src/rest/LogRESTService.java
@@ -1,15 +1,13 @@
 package rest;
 
-import java.util.logging.LoggingMXBean;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import bean.ArticulosBean;
 import beans.LogBean;
 
 @Stateless
@@ -26,8 +24,17 @@ public class LogRESTService {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/all")
-	public String getBestSellers() {
-		//return logBean.getBestSellers();
+	public String getLogs() {
+		
+		return logBean.getLogs();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{modulo}")
+	public String getLogsPorModulo(@PathParam("modulo") String modulo) {
+		
+		return logBean.getLogsPorModulo(modulo);
 	}
 
 }
